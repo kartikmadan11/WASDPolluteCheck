ef0316cde28d11ab73bf42697b635019b7cea110
Parth Chopra <parth.chopra04@gmail.com>
2018-09-30 19:59:39 +0000

    Added Sensor and Bluetooth COmmunication

--- Arduino/ArduinoBluetoothSerial.ino
+++ Arduino/ArduinoBluetoothSerial.ino
@@ -1,14 +1,23 @@
+#include<SoftwareSerial.h>
+int smoke=0;
 int ppm=0;
 int noise=0;
 
+SoftwareSerial BT(2,3);
 void setup() {
-  pinMode(4,OUTPUT);
-  pinMode(A0,OUTPUT);
+  BT.flush();
+  pinMode(4,INPUT);
+  pinMode(A1,INPUT);
+  pinMode(A0,INPUT);
   Serial.begin(9600);
+  BT.begin(9600);
 }
 
 void loop() {
  ppm=analogRead(A0);
  noise=digitalRead(4);
- Serial.println(String(ppm)+","+String(noise));
+ smoke=analog
+ Read(A1);
+ BT.println(String(ppm)+","+String(smoke)+","+String(noise));
+ delay(2000);
 }
