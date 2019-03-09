c653b30ff5455c2efb2b3c931544dcacdb35a508
Kartik Madan <magiciankartik00@gmail.com>
2019-03-09 15:51:33 +0530

    Update PiToFirebase.py

--- raspberry-pi/PiToFirebase.py
+++ raspberry-pi/PiToFirebase.py
@@ -2,6 +2,6 @@ from firebase import firebase
 
 firebase = firebase.FirebaseApplication('https://wasdpollutecheck.firebaseio.com/', None)
 
-myObject = {'factoryID101': {'factoryName': 'Tata Sons', 'factoryEstablished' : '1956', 'smokePPM': 272, 'noisePollution': 1, 'irSensor': 400, 'phSensor': 2.7} }
+myObject = {'factoryID101': {'factoryName': 'Tata Sons', 'factoryEstablished' : '1956', 'smokePPM': 272, 'noisePollution': 0, 'irSensor': 400, 'phSensor': 2.7} }
 
 firebase.put('https://wasdpollutecheck.firebaseio.com/', 'factories', myObject)
