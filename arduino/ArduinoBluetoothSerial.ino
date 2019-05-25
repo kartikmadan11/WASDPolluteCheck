3dfa51bcb8585e2951aa8d84e781482e7f644c05
Parth Chopra <parth.chopra04@gmail.com>
2019-05-25 19:37:22 +0530

    mq136, mq7 and temperature sensor added

--- arduino/ArduinoBluetoothSerial.ino
+++ arduino/ArduinoBluetoothSerial.ino
@@ -1,24 +1,39 @@
 #include<SoftwareSerial.h>
+#include <OneWire.h> 
+#include <DallasTemperature.h>
+
+#define ONE_WIRE_BUS 5
+OneWire oneWire(ONE_WIRE_BUS);
+
+DallasTemperature sensors(&oneWire);
+
 int smoke=0;
-int ppm=0;
+int mq136=0;
+int mq7=0;
 int noise=0;
+int temp=0;
 
 SoftwareSerial BT(2,3);
 void setup() {
   BT.flush();
-  pinMode(4,INPUT);
-  pinMode(A1,INPUT);
-  pinMode(A0,INPUT);
-  pinMode(5,INPUT);
+  pinMode(4,INPUT);             // Sound
+  pinMode(3,INPUT);            // IR
+  pinMode(A0,INPUT);          // MQ136
+  pinMode(5,INPUT);           // DS18B20
+  pinMode(A1,INPUT);          // MQ7
   Serial.begin(9600);
   BT.begin(9600);
+  sensors.begin();
 }
 
 void loop() {
- ppm=analogRead(A0);
+  sensors.requestTemperatures();
+ mq136=analogRead(A0);        
  noise=digitalRead(4);
- smoke=analogRead(A1);
- motion=digitalRead(5);
- BT.println(String(ppm)+","+String(smoke)+","+String(noise));
- delay(1000);
+ smoke=digitalRead(3);
+ temp=sensors.getTempCByIndex(0);
+ mq7=analogRead(A1);
+ Serial.println("MQ136:"+String(mq136)+","+"IR"+String(smoke)+","+"Sound Sensor:"+String(noise)+","+"MQ7:"+String(mq7)+","+"Temperature:"+String(temp));
+ BT.println(String(mq136)+","+String(smoke)+","+String(noise)+","+String(mq7)+","+String(temp));
+ delay(5000);
 }
